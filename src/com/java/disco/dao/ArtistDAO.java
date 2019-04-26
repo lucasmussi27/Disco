b6ad6624e617ad49cbc9d6f189ae911013efca12
lucasmussi27 <lucas.mussi27@outlook.com>
2019-04-25 22:16:45 -0300

    Disco - v. JAVA - Dao Modified

--- src/com/java/disco/dao/ArtistDAO.java
+++ src/com/java/disco/dao/ArtistDAO.java
@@ -26,9 +26,53 @@ public class ArtistDAO {
         }
     }
 
-    public List<ArtistDTO> read() {}
+    public List<ArtistDTO> read() {
+        String sql = "select * from artist";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<ArtistDTO> list = new ArrayList<>();
+        try {
+            stmt = this.conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while(rs.next()) {
+                ArtistDTO a = new ArtistDTO();
+                a.setId(rs.getInt("id"));
+                a.setName(rs.getString("name"));
+                a.setGenre(rs.getString("genre"));
+                list.add(a);
+            }
+        } catch(SQLException se) {
+            System.err.println(se.getCause());
+            System.err.println(se.getMessage());
+        }
+        return list;
+    }
 
-    public void update() {}
+    public void update(ArtistDTO art) {
+        String sql = "update artist set name = ?, genre = ? where id = ?";
+        PreparedStatement stmt = null;
+        try {
+            stmt = this.conn.prepareStatement(sql);
+            stmt.setInt(1, art.getId());
+            stmt.setString(2, art.getName());
+            stmt.setString(3, art.getGenre());
+            stmt.executeUpdate();
+        } catch(SQLException se) {
+            System.err.println(se.getCause());
+            System.err.println(se.getMessage());
+        }
+    }
 
-    public void delete() {}
+    public void delete(int id) {
+        String sql = "delete artist where id = ?";
+        PreparedStatement stmt = null;
+        try {
+            stmt = this.conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch(SQLException se) {
+            System.err.println(se.getCause());
+            System.err.println(se.getMessage());
+        }
+    }
 }
